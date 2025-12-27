7cc320d4bc19554d2fa43d221e36d97656b258ed
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-12-27 12:10:12 -0300

    gemini-cli: 0.22.2 -> 0.22.4
    (cherry picked from commit 776341a9e3afe4e8b538974823a6e0cc26f4e0a0)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3806,6 +3806,13 @@
     github = "brancz";
     githubId = 4546722;
   };
+  brantes = {
+    name = "Pedro Brantes";
+    email = "contact@brantes.simplelogin.com";
+    github = "pedrobrantes";
+    githubId = 58346706;
+    matrix = "@brantes:matrix.org";
+  };
   braydenjw = {
     email = "nixpkgs@willenborg.ca";
     github = "braydenjw";
