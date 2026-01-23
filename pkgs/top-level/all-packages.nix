1b5c9e7bf760f3ef413da0aa46c319b0d6b1f69a
Fernando Rodrigues <alpha@sigmasquadron.net>
2026-01-24 10:20:49 +1100

    fetchFromForgejo: init
    Simple alias, as Gitea's fetcher is compatible with Forgejo.
    
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>
    
    (cherry picked from commit adf7a4850b7d89c0658bdae2daff9680832680a7)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -664,6 +664,8 @@ with pkgs;
 
   fetchFromGitea = callPackage ../build-support/fetchgitea { };
 
+  fetchFromForgejo = fetchFromGitea;
+
   fetchFromCodeberg = callPackage ../build-support/fetchcodeberg { };
 
   fetchFromGitHub = callPackage ../build-support/fetchgithub { };
